6b61e38a368ecfbcbd767514ba7c1ce0962ce260
biran-envops <biran@envops.com>
2022-03-17 23:46:43 +0200

    made rest api, changed tests

---
+++ restapi/api.js
@@ -0,0 +1,34 @@
+
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const tests = require('../tests.js');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+
+// Configuring body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/api/tests/testConnection', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const client = req.body.client;
+    const result = () => {
+        tests.testUserConnection(username, password, client)
+            .then(errorCode => {
+                res.send(errorCode);
+            });
+    }
+    result();
+});
+
+
+
+app.get('/api/', (req, res) => {
+});
+
+app.listen(port, () => console.log(`testing server listening on port ${port}!`));
---
+++ tests.js
@@ -0,0 +1,59 @@
+
+function testUserConnection(username, password, clientName) {
+  return new Promise(function (resolve, reject) {
+    const playwright = require('playwright');
+
+    function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    let errorCode;
+
+    (async () => {
+      const browser = await playwright.chromium.launch({ headless: true });
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      await page.goto('http://ec2-54-87-66-8.compute-1.amazonaws.com:32001/guacamole/#/');
+      await sleep(3000);
+      await page.keyboard.type(username);
+      await page.keyboard.press('Tab');
+      await page.keyboard.type(password);
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      try {
+        await page.click(`:text("${clientName}")`, {
+          timeout: 4000,
+        });
+      } catch (error) {
+        if (error instanceof playwright.errors.TimeoutError) {
+          console.log("No connection by that name");
+          await browser.close();
+          errorCode = "2: No client with that name";
+          resolve(errorCode);
+        }
+      }
+      if (errorCode == undefined) {
+        await sleep(6000);
+        const errorCount = await page.locator(':text("Connection Error")').count();
+        await browser.close();
+        if (errorCount > 0) {
+          console.log("Connection Error, See guacamole logs for info.");
+          errorCode = "1: Connection Error, See guacamole logs for info.";
+          resolve(errorCode);
+        } else {
+          console.log("Connection Successful");
+          errorCode = "0: Connection Successful";
+          resolve(errorCode);
+        }
+      }
+
+    })();
+  });
+}
+
+module.exports = { testUserConnection };
+
+
+
+
+
